b83409fa7cf136c99e6ccc2071be9fc3af03106f
Brian Cosgrove <cosgroveb@gmail.com>
2025-04-14 15:04:31 -0500

    WIP - \ff behaves like vim_dotfiles (#39)
    * first try and reversing order
    
    * wip fallback
    
    * maybe
    
    * Move search bar to top
    
    * use recipe hector found instead

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -2,6 +2,25 @@
 
 local SwitchColorschemeKeyMap = require("lazy_utils").SwitchColorschemeKeyMap
 
+-- cache results of rev-parse
+local is_inside_work_tree = {}
+
+function vim.find_files_from_project_git_root()
+  local opts = {} -- define here if you want to define something
+
+  local cwd = vim.fn.getcwd()
+  if is_inside_work_tree[cwd] == nil then
+    vim.fn.system("git rev-parse --is-inside-work-tree")
+    is_inside_work_tree[cwd] = vim.v.shell_error == 0
+  end
+
+  if is_inside_work_tree[cwd] then
+    require("telescope.builtin").git_files(opts)
+  else
+    require("telescope.builtin").find_files(opts)
+  end
+end
+
 return {
 	{
 		"nvim-lualine/lualine.nvim",
@@ -35,9 +54,9 @@ return {
 			},
 		},
 		keys = {
-			{ "<Leader><Leader>", "<cmd>Telescope find_files<CR>", desc = "Find files" },
-			{ "<Leader>ff", "<cmd>Telescope find_files<CR>", desc = "Find files" },
-			{ "<C-p>", "<cmd>Telescope find_files<CR>", desc = "Find files" },
+			{ "<Leader><Leader>", "<cmd>lua vim.find_files_from_project_git_root()<CR>", desc = "Find files" },
+			{ "<Leader>ff", "<cmd>lua vim.find_files_from_project_git_root()<CR>", desc = "Find files" },
+			{ "<C-p>", "<cmd>lua vim.find_files_from_project_git_root()<CR>", desc = "Find files" },
 			{ "<Leader>fg", "<cmd>Telescope live_grep<CR>", desc = "Find (grep)" },
 			{ "<Leader>be", "<cmd>Telescope buffers<CR>", desc = "Buffer Explorer" },
 			{ "<Leader>gw", "<cmd>Telescope grep_string<CR>", desc = "Grep word in cursor" },
@@ -49,6 +68,7 @@ return {
 			defaults = {
 				sorting_strategy = "ascending",
 				layout_config = {
+					prompt_position = 'top',
 					horizontal = {
 						width = { padding = 0 },
 						height = { padding = 0 },
