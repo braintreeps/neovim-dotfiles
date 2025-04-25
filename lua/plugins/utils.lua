f05b043525543ba44bb631ad388c15b86438122a
Nick deLannoy <nick.delannoy@gmail.com>
2025-04-24 20:17:01 -0500

    Set up `windows.nvim` (#81)

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -42,4 +42,27 @@ return {
 			{ "<leader>uZ", function() Snacks.zen() end, desc = "[Z]en mode" },
 		}
 	},
+	{
+		"anuvyklack/windows.nvim",
+		dependencies = {
+			"anuvyklack/middleclass",
+			"anuvyklack/animation.nvim",
+		},
+		config = function()
+			vim.o.winwidth = 10
+			vim.o.winminwidth = 10
+			vim.o.equalalways = false
+			require("windows").setup()
+		end,
+		keys = {
+			{ "<leader>wm", "<Cmd>WindowsMaximize<CR>",  desc = "Maximize window (toggle)" },
+			{ "<leader>wh", "<Cmd>WindowsMaximizeHorizontally<CR>",  desc = "Maximize window horizontally" },
+			{ "<leader>wv", "<Cmd>WindowsMaximizeVertically<CR>",  desc = "Maximize window vertically" },
+			{ "<leader>we", "<Cmd>WindowsEqualize<CR>",  desc = "Equalize windows" },
+			{ "<leader>wa", "<Cmd>WindowsToggleAutowidth<CR>",  desc = "Toggle window autowidth" },
+			{ "<C-w>a", "<Cmd>WindowsToggleAutowidth<CR>",  desc = "Toggle window autowidth" },
+			{ "<leader>ww", "<C-W>p", desc = "Other Window", remap = true },
+			{ "<leader>wd", "<C-W>c", desc = "Delete Window", remap = true },
+		}
+	}
 }
