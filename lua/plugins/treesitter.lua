1001e9dcb49d5c4bc52bf7024b695d2e916d1f7b
Nick deLannoy <nick.delannoy@gmail.com>
2025-06-27 16:29:27 -0400

    Treesitter fixes (#179)
    * add treesitter textobjects and fix some other treesitter issues
    
    ---------

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,11 +6,47 @@ local treesitter_highlight_max_file_size = 150 -- Kilobytes
 return {
     {
         "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdateSync",
-        event = { "BufNewFile", "BufReadPost" },
+        version = false, -- last release is way too old and doesn't work on Windows
+        build = ":TSUpdate",
+        event = { "VeryLazy" },
+        cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
+        init = function(plugin)
+            -- PERF: add nvim-treesitter queries to the rtp and load custom query predicates early
+            -- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
+            -- no longer trigger the **nvim-treesitter** module to be loaded in time.
+            -- Luckily, the only things that those plugins need are custom queries, which we make available
+            -- during startup.
+            require("lazy.core.loader").add_to_rtp(plugin)
+            require("nvim-treesitter.query_predicates")
+        end,
         dependencies = {
             "RRethy/nvim-treesitter-endwise",
             "RRethy/vim-illuminate",
+            {
+                "nvim-treesitter/nvim-treesitter-textobjects",
+                config = function()
+                    -- When in diff mode, we want to use the default
+                    -- vim text objects c & C instead of the treesitter ones.
+                    local move = require("nvim-treesitter.textobjects.move") ---@type table<string,fun(...)>
+                    local configs = require("nvim-treesitter.configs")
+                    for name, fn in pairs(move) do
+                        if name:find("goto") == 1 then
+                            move[name] = function(q, ...)
+                                if vim.wo.diff then
+                                    local config = configs.get_module("textobjects.move")[name] ---@type table<string,string>
+                                    for key, query in pairs(config or {}) do
+                                        if q == query and key:find("[%]%[][cC]") then
+                                            vim.cmd("normal! " .. key)
+                                            return
+                                        end
+                                    end
+                                end
+                                return fn(q, ...)
+                            end
+                        end
+                    end
+                end,
+            },
         },
         opts = {
             highlight = {
@@ -36,6 +72,22 @@ return {
                 enable = true,
             },
             textobjects = {
+                select = {
+                    enable = true,
+                    keymaps = {
+                        ["ac"] = { query = "@class.outer", desc = "Select around class" },
+                        ["ic"] = { query = "@class.inner", desc = "Select inside class" },
+                        ["af"] = { query = "@function.outer", desc = "Select around function" },
+                        ["if"] = { query = "@function.inner", desc = "Select inside function" },
+                        ["al"] = { query = "@loop.outer", desc = "Select around loop" },
+                        ["il"] = { query = "@loop.inner", desc = "Select inside loop" },
+                        ["ab"] = { query = "@block.outer", desc = "Select around block" },
+                        ["ib"] = { query = "@block.inner", desc = "Select inside block" },
+                    },
+                    selection_modes = {
+                        ["@class.outer"] = "V",
+                    },
+                },
                 move = {
                     enable = true,
                     goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer" },
@@ -85,8 +137,7 @@ return {
             },
         },
         config = function(_, opts)
-            local config = require("nvim-treesitter.configs")
-            config.setup(opts)
+            require("nvim-treesitter.configs").setup(opts)
         end,
     },
 }
