885f0cd1995e544f2e1ad60526894ee9929e63cc
Lairen Hightower <lairenhightower@gmail.com>
2025-10-17 09:59:37 -0700

    Add pry-bybug to \rd shortcut (#240)

--- ftplugin/ruby.lua
+++ ftplugin/ruby.lua
@@ -1,5 +1,5 @@
 -- mnemonic: ruby-debug
-vim.keymap.set("", "<LocalLeader>rd", "Orequire 'pry'; binding.pry<ESC>", { desc = "[r]uby [d]ebug: binding.pry line under cursor" })
+vim.keymap.set("", "<LocalLeader>rd", "Orequire 'pry'; require 'pry-byebug'; binding.pry<ESC>", { desc = "[r]uby [d]ebug: binding.pry line under cursor" })
 vim.keymap.set("n", "<silent> <LocalLeader>rs", ":!ruby -c %<CR>", { desc = "[r]uby [s]yntax: check the syntax of the current file" })
 
 local function remove_binding_pry()
